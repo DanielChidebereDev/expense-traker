29c842753a4c52037798030d710f69218bab0027
Daniel Chidebere <dhzdynamic@gmail.com>
2022-11-02 06:08:28 +0100

    mern stack

---
+++ backend/config/database.js
@@ -0,0 +1,1 @@
+module.exports = connect2DB;
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
 });
 
-mongoose
-  .connect(
-    'mongodb+srv://danielDev:505050@expense-app.iuwdnmn.mongodb.net/?retryWrites=true&w=majority'
-  )
-  .then(() => {
-    console.log('connected successful');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.listen(4000, () =>
-  console.log('server is running at http://localhost:4000')
+await mongoose.connect(
+  'mongodb+srv://danielDev:505050@expense-app.iuwdnmn.mongodb.net/?retryWrites=true&w=majority'
 );
+
+console.log(`connected: successfully`);
+
+const Port = process.env.PORT || 5000;
+app.listen(Port, () => {
+  console.log(
+    `these application is serve at localhost http://localhost:${Port}`
+  );
+});
